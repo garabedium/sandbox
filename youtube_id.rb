fc3775651227b0bafeb9da5e9d26094fc04a3467
Greg Garabedian <ggarabedian09@gmail.com>
2018-05-23 21:45:48 -0400

    youtube id challenge setup

---
+++ youtube_id.rb
@@ -0,0 +1,15 @@
+# Generate YouTube IDs:
+  # Challenge that I made up to create random YouTube style IDs
+  # The results aren't truly random, it's just an exercise!
+
+  # Write a method that takes two arguments:
+    # length of the generated id, and total number of ids to generate
+  # Generate unique alphanumeric ids based on the method's arguments
+  # Don't duplicate ids
+  # Return hash of unique ids
+
+  def generate_ids(id_length,total_ids)
+    ids = {}
+  end
+
+  generate_ids(10,5) # Generate 5 unique IDs with a length of 10
