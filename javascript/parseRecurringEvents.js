6e6650a119f500f9bcccf877dc7fe6a9b6083fcb
Greg Garabedian <ggarabedian09@gmail.com>
2018-06-11 23:42:19 -0400

    parseRecurringEvents challenge

---
+++ javascript/parseRecurringEvents.js
@@ -0,0 +1,8 @@
+// Parse Event Recurrences
+// Client project where event recurrence rules were stored as strings in the database
+// Strings were parsed and displayed with JavaScript
+
+// Input: INTERVAL=1;BYDAY=TH
+// Output: Thursdays
+// Input: INTERVAL=1;BYDAY=MO,SA
+// Output: Mondays, Saturdays:
