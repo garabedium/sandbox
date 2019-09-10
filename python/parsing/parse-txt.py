dbc42208b99f97aa060f9af26a5971859b46fc6a
garabedium <ggarabedian09@gmail.com>
2019-09-10 00:10:31 -0400

    read lines via for loop

--- python/parsing/parse-txt.py
+++ python/parsing/parse-txt.py
@@ -5,12 +5,16 @@ words = []
 
 with open('common-words-sample.txt') as fp:
   line = fp.readline()
-  line_count = 0
-  while line_count < 20:
-    line = fp.readline().strip().split('\t')
-    word = { "word": line[0], "frequency": int(line[1]) }
-    words.append(word)
-    line_count += 1
+  count = 0
+  for line in fp:
+    line = line.strip().split('\t')
+    word = line[0]
+    if len(word) == 6:
+      item = { "word": word, "frequency": int(line[1]) }
+      words.append(item)
+    count += 1
 
 with open('sample-words-parsed.json','w') as fp:
   json.dump(words,fp)
+
+print(len(words))
