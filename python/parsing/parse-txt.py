c3875d018fcd4120ee5cee5636bbf13e62da43f3
garabedium <ggarabedian09@gmail.com>
2019-09-10 00:12:59 -0400

    added comments

--- python/parsing/parse-txt.py
+++ python/parsing/parse-txt.py
@@ -3,17 +3,20 @@ import json
 
 words = []
 
+# Read txt file and select all 6 letter words
 with open('common-words-sample.txt') as fp:
   line = fp.readline()
+  word_length = 6
   count = 0
   for line in fp:
     line = line.strip().split('\t')
     word = line[0]
-    if len(word) == 6:
+    if len(word) == word_length:
       item = { "word": word, "frequency": int(line[1]) }
       words.append(item)
     count += 1
 
+# Write words array to JSON file
 with open('sample-words-parsed.json','w') as fp:
   json.dump(words,fp)
 
